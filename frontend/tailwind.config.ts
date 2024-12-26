78666c5f0a206e9ab6063f71109181ba8ff93cdb
user.email <div.singh.rathore.ff@gmail.com>
2024-12-27 04:49:59 +0530

    added frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['var(--playfair-display-font)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
