c12541c7ab7d24a1f7e33fa925ef934def2a7480
user.email <div.singh.rathore.ff@gmail.com>
2024-12-28 03:00:37 +0530

    added create zap page

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { userRouter } from './routes/user'
 import { zapRouter } from './routes/zap'
 import cors from 'cors'
+import { actionRouter } from './routes/action'
+import { triggerRouter } from './routes/trigger'
 
 const app = express()
 app.use(express.json())
@@ -9,6 +11,8 @@ app.use(cors())
 
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/zap', zapRouter)
+app.use('/api/v1/actions', actionRouter)
+app.use('/api/v1/trigger', triggerRouter)
 
 app.listen(3333, () => {
   console.log('Listening on port 3333')
